7344688ef49b165e6940d5f434385362de1438da
ikosong14 <alexandervirtuous14@gmail.com>
2024-10-23 23:09:43 +0100

    modified:   app/components/Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,15 +6,15 @@ import logo from "../images/logo.png"; // Make sure this path matches your logo 
 export default function Footer() {
   return (
     <footer className="bg-blue text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:items-center px-4 md:px-8 lg:px-16">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-8 lg:px-16">
         {/* Logo & Site Info */}
-        <div className="mb-6 md:mb-0 flex flex-col items-start">
+        <div className="mb-6 md:mb-0 flex flex-col items-center md:items-start text-center md:text-left">
           <Image src={logo} alt="BitCloud Logo" width={50} height={50} />
           <p className="text-sm mt-4 text-gray-400">Drive Your Investment</p>
         </div>
 
         {/* Navigation Links */}
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 mb-6 md:mb-0">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 mb-6 md:mb-0 items-center">
           <Link
             href="/about"
             className="text-gray-400 hover:text-blue-400 transition"
